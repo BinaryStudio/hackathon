e2bf151cc30d9a29f6c2fc084264b95671cdd6bb
yijingbai <everatnight@gmail.com>
2014-08-02 18:42:13 +0800

    add init.py

--- api.py
+++ api.py
@@ -1,18 +1,25 @@
 import sys
-from flask import Flask, request
-from flask.ext.restful import Resource, Api
+from flask import Flask
+from flask.ext.restful import Api
 from resources.parking import Parking, Parkings
+from resources.slot import SlotInc
+
 from lib.utils.ext import db
+
 
 def create_app():
     app = Flask('parkplaceapi')
     app.config.from_pyfile('api.config.' + sys.argv[1] + '.cfg', silent=True)
-    api = Api(app)
+
     db.init_app(app)
-    with app.app_context():
+    with app.app_cotext():
         db.create_all()
+
+    api = Api(app)
     api.add_resource(Parking, '/parkings/<string:parking_id>')
     api.add_resource(Parkings, '/parkings')
+    api.add_resource(SlotInc, '/slot/<string:parking_id>')
+
     return app
 
 app = create_app()
