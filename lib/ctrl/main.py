63d8da85d486b848739e4cd022ec05acf10bce8b
yijing bai <everatnight@gmail.com>
2014-08-03 00:24:52 +0800

    ad

--- lib/ctrl/main.py
+++ lib/ctrl/main.py
@@ -6,3 +6,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/map')
+def map():
+    return render_template('map.html')
