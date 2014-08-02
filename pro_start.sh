4a28146d5786067434ca85588e99e00b927f03ba
MidnightLee <mylimidnight@gmail.com>
2014-08-02 23:21:32 +0800

    update shell scripts

--- pro_start.sh
+++ pro_start.sh
@@ -1,2 +1,8 @@
+rm -rf *.out
 nohup python api.py pro >> api.out &
 nohup python app.py pro >> app.out &
+sleep 1
+echo '-----'
+cat api.out
+echo '-----'
+cat app.out
--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
+rm -rf *.out
 nohup python api.py dev >> api.out &
 nohup python app.py dev >> app.out &
+sleep 1
+echo '-----'
+cat api.out
+echo '-----'
+cat app.out
+
