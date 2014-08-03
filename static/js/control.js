ed1ac2c6263660a04656120c8cbe4e4e01b2e41e
yijingbai <everatnight@gmail.com>
2014-08-03 14:44:06 +0800

    change event

--- static/js/control.js
+++ static/js/control.js
@@ -1,14 +1,14 @@
-$(function () {
-	var id = $('#placeid').html();
-	$('#carin').touch(function (event) {
-		console.log('clickin');
-		$.get('/rest/slots/inc/' + id, function () {});
-	});
-	$('#carout').touch(function (event) {
-		console.log('clickout');
-		$.get('/rest/slots/des/' + id, function () {});
-	});
-	$.get('/rest/parkings/' + id, function (msg) {
-		console.log(msg);
-	})
+$(function() {
+    var id = $('#placeid').html();
+    $('#carin').on('touch', function(event) {
+        console.log('clickin');
+        $.get('/rest/slots/inc/' + id, function() {});
+    });
+    $('#carout').on('touch', function(event) {
+        console.log('clickout');
+        $.get('/rest/slots/des/' + id, function() {});
+    });
+    $.get('/rest/parkings/' + id, function(msg) {
+        console.log(msg);
+    })
 });
