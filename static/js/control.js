192b0115483f8900069f4073dfa12323875fcac7
yijingbai <everatnight@gmail.com>
2014-08-03 15:22:26 +0800

    change

--- static/js/control.js
+++ static/js/control.js
@@ -2,13 +2,16 @@ $(function() {
     var id = $('#placeid').html();
     $('#carin').on('click', function(event) {
         console.log('clickin');
-        $.get('/rest/slots/inc/' + id, function() {});
+        $.get('/rest/slots/des/' + id, function() {});
     });
     $('#carout').on('click', function(event) {
         console.log('clickout');
-        $.get('/rest/slots/des/' + id, function() {});
+        $.get('/rest/slots/inc/' + id, function() {});
     });
     $.get('/rest/parkings/' + id, function(msg) {
         console.log(msg);
+        $('#parkname').html(msg.name);
+        $('#total_pak').html(msg.total_pak);
+
     })
 });
