cf031d67bcbfbf81a78bae2255e4075a7cdcab7e
yijing bai <everatnight@gmail.com>
2014-08-03 12:43:33 +0800

    update

--- static/js/control.js
+++ static/js/control.js
@@ -1,8 +1,10 @@
 $(function () {
 	$('#carin').click(function (event) {
+		console.log('clickin');
 		submitData('get', '/rest/slots/inc/', form, callBackGenerator('success'), callBackGenerator('error'));
 	});
 	$('#carout').click(function (event) {
+		console.log('clickout');
 		submitData('get', '/rest/slots/des/', form, callBackGenerator('success'), callBackGenerator('error'));
 	});
 });
